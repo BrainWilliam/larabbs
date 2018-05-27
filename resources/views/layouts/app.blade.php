da0d1f2313d69a47da5810157b7243ad6ced95bb
brianwilliam <1692769645@qq.com>
2018-05-27 14:40:45 +0000

    finished basic page layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <title> @yield('title','laraBBS') -Laravel官网BBS系统 </title>
+        <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+        <meta name="csrf_token" content="{{csrf_token()}}">
+    </head>
+    <body>
+        <div id='app' class="{{route_class()}}-page">
+            @include('layouts/_header')
+                <div class="container">
+                    @yield('content')
+                </div>
+            @include('layouts/_footer')
+        </div>
+        <script src="{{asset('js/app.js')}}">
+        </script>
+    </body>
+</html>
