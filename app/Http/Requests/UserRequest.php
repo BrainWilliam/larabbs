b6feb0780a14fd64342b394a6c742b485b4142bc
brianwilliam <1692769645@qq.com>
2018-05-28 11:55:37 +0000

    add user profile

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Auth;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+          'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+          'email' => 'required|email',
+          'introduction' => 'max:80',
+        ];
+    }
+
+    public function messages(){
+        return [
+            'name.unique'=>'用户名已经被占用，请重新填写',
+            'name.regex'=>'用户名不符合规范',
+            'name.between'=>'用户名介于3-25之间',
+            'name.required'=>'用户名必填',
+        ];
+    }
+}
---
+++ resources/views/layouts/_message.blade.php
@@ -0,0 +1,18 @@
+@if (Session::has('message'))
+<div class="alert alert-info">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+    {{ Session::get('message') }}
+</div>
+@endif
+@if (Session::has('success'))
+<div class="alert alert-success">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+    {{ Session::get('success') }}
+</div>
+@endif
+@if (Session::has('error'))
+<div class="alert alert-danger">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+    {{ Session::get('error') }}
+</div>
+@endif
