1f218bfd87a3101fbbc54e60f0b763a9cd5f2f04
brianwilliam <1692769645@qq.com>
2018-05-28 13:16:54 +0000

    user can upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,20 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
-    public function show(User $user){
-        return view('users.show',compact('user'));
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
-    public function edit(User $user){
-        return view('users.edit',compact('user'));
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
     }
-    public function update(UserRequest $request,User $user){
-        $user->update($request->all());
-        return redirect()->route('users.show',$user->id)->with('success','用户信息修改成功！');
+    public function update(UserRequest $request, User $user, ImageUploadHandler $file)
+    {
+        $data = $request->all();
+        if ($request->avatar) {
+            $fileName = $file->save($request->avatar, 'avatar', $user->id,250);
+            if ($fileName) {
+                $data['avatar'] = $fileName['path'];
+            }
+        }
+        $user->update($data);
+        return redirect()->route('users.show', $user->id)->with('success', '用户信息修改成功！');
     }
 }
--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -26,8 +26,9 @@ class UserRequest extends FormRequest
     {
         return [
           'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
-          'email' => 'required|email',
+          'email' => 'required|email|unique:users,email,'.Auth::id(),
           'introduction' => 'max:80',
+          'avatar'=>'mimes:jpg,png,bmp,gif,jpeg|dimensions:min_width=200,min_height=200'
         ];
     }
 
@@ -37,6 +38,8 @@ class UserRequest extends FormRequest
             'name.regex'=>'用户名不符合规范',
             'name.between'=>'用户名介于3-25之间',
             'name.required'=>'用户名必填',
+            'avatar.mimes'=>'请上传正确的图片！',
+            'avatar.dimensions'=>'叔叔，你像素咋这么渣！'
         ];
     }
 }
