7190254bc54858be2afadf538fa365c4173ff262
brianwilliam <1692769645@qq.com>
2018-05-28 14:15:24 +0000

    add authorize

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,16 +8,21 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth',['except'=>'show']); //首选except 这是最佳实践
+    }
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit', compact('user'));
     }
     public function update(UserRequest $request, User $user, ImageUploadHandler $file)
     {
+        $this->authorize('update',$user);
         $data = $request->all();
         if ($request->avatar) {
             $fileName = $file->save($request->avatar, 'avatar', $user->id,250);
