ae082eba5654b89f5fa017e4f9ddc4824d303cc9
brianwilliam <1692769645@qq.com>
2018-05-29 15:41:57 +0000

    topic index page

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,15 +2,19 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        $faker = app(Faker\Generator::class);
+        $user_ids = User::all()->pluck('id')->toArray();
+        $category_ids = Category::all()->pluck('id')->toArray();
+        $topics = factory(Topic::class)->times(100)->make()->each(function ($topic, $index) use($faker,$user_ids,$category_ids) {
+            $topic->user_id = $faker->randomElement($user_ids);
+            $topic->category_id = $faker->randomElement($category_ids);
         });
 
         Topic::insert($topics->toArray());
